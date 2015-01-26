67c793fc97d130f0eaced5dcad4bc9eda88af4ac
tannerd <tannerd@mail.gvsu.edu>
2015-01-26 14:42:03 -0500

    allows multiple clients to connect
    each client can now send a message to the server, but does not get the
    messages from the other clients

--- Lab03_Client.c
+++ Lab03_Client.c
@@ -1,63 +1,40 @@
-#include<stdio.h> //printf
-#include<string.h>    //strlen
-#include<sys/socket.h>    //socket
-#include<arpa/inet.h> //inet_addr
-//
 
-int main(int argc , char *argv[])
-{
-    int sock;
-    struct sockaddr_in server;
-    char message[1000] , server_reply[2000];
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
 
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
+int main(int argc, char** argv){
 
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 8888 );
+  int sockfd = socket(AF_INET,SOCK_STREAM,0);
+  if(sockfd<0){
+    printf("There was an error creating the socket\n");
+    return 1;
+  }
 
-    //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
-        return 1;
-    }
+  struct sockaddr_in serveraddr;
+  serveraddr.sin_family=AF_INET;
+  serveraddr.sin_port=htons(9876);
+  serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
 
-    puts("Connected\n");
-    puts("Chatroom.");
+  int e = connect(sockfd,(struct sockaddr*)&serveraddr,
+  sizeof(struct sockaddr_in));
+  if(e<0){
+    printf("There was an error with connecting\n");
+    return 1;
+  }
 
-    //keep communicating with server
-    while(1)
-    {
+  printf("Enter a line: ");
+  char line[5000];
+  fgets(line,5000,stdin);
+  send(sockfd,line,strlen(line),0);
+  char line2[5000];
+  int n = recv(sockfd,line2,5000,0);
+  if(n<0){
+    printf("Sorry, had a problem receiving\n");
+    return 1;
+  }
+  printf("Got from server: %s\n",line2);
 
-        printf("Enter message: ");
-    fgets(message, sizeof(message),stdin);
-        //scanf("%s" , message);
-
-        //Send some data
-        if( send(sock , message , strlen(message) , 0) < 0)
-        {
-            puts("Send failed");
-            return 1;
-        }
-
-        //Receive a reply from the server
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
-
-    printf("Server Reply: %s\n", server_reply);
-    server_reply[0]='\0';
-    }
-
-    close(sock);
-    return 0;
+  return 0;
 }
